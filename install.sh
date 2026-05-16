95800bc40b9d64253ecd86a5f14967a5576aa8a9
Reza Mirzaee <rmirzaee98@gmail.com>
2026-05-16 11:30:00 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ set -euo pipefail
 readonly AVC_BUILD_ID="avc-7f3a92e1-2025-avacocloud"
 export AVC_BUILD_ID
 
-REPO_URL="https://github.com/avacocloud/XHTTP-Installer.git"
+REPO_URL="https://github.com/r-mzy47/XHTTP-Installer.git"
 TARGET_DIR="/root/XHTTP-Installer"
 BRANCH="main"
 
